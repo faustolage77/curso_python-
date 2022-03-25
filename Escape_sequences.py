680fbbf851edb4042407b798c849665701fc2f35
faustolage77 <100029945+faustolage77@users.noreply.github.com>
2022-03-25 16:41:32 -0300

    Add files via upload

---
+++ Escape_sequences.py
@@ -0,0 +1,38 @@
+#   Escape sequences
+
+# print("D:\\Lessons\\Programming")
+# print("Prints \n in two lines ")
+
+######
+ #
+ # Sets
+
+# s1 = {'ab', 3, 4, (5, 6)}  #  um set contendo uma tupla
+# s2 = {'ab', 7, (7, 6)}  #  outro set contendo uma tupla
+# # print(s1 - s2)
+# #
+# print(s1.intersection(s2))  #  aqui o comando manda imprimir somente a intersecao entre meus 2 sets
+#  ou seja, somente o que tem em um E  em outro
+# s1[2] = 2  #  por exemplo eu nao poderia fazer isso pois os elementos dentro de um set sao imutaveis
+#  digamos que vc tem varios grupos e conjuntos e dados, se quiser descobrir o que ha de comum neles
+#  basta usar o metodo de entrada x.intersection()
+
+# print(s1.union(s2))  #  aqui o comando faz a uniao entre meus 2 sets
+#
+# print('ab' in s1)  #  aqui é uma operacao booleana, estou perguntando se ab está contido no s1
+#
+# print('ab' not in s1 )
+#
+# for element in s1:
+#     print(element)
+
+########
+
+#  frozen sets
+#
+# fs1 = frozenset(s1)
+# fs2 = frozenset(s2)
+#
+# print({fs1: 'fs1', fs2:'fs2'})
+
+
